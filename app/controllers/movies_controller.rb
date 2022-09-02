b4f55c12674723dd57e0dd73b52d7ea06f0d5ed6
Jaime Montes de Oca <jaime0422@live.com>
2022-09-02 00:21:00 -0700

    Completed Assignment

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,19 +1,30 @@
 class MoviesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
-  def index
-    movies = Movie.all
-    render json: movies
-  end
+    def index
+        movies = Movie.all
+        render json: movies
+    end
 
-  def show
-    movie = Movie.find(params[:id])
-    render json: movie.to_json(except: [:created_at, :updated_at])
-  end
+    def summaries
+        movies = Movie.all
+        render json: movies, each_serializer: MovieSummarySerializer
+    end
 
-  private
+    def show
+        movie = Movie.find(params[:id])
+        render json: movie
+    end
 
-  def render_not_found_response
-    render json: { error: "Movie not found" }, status: :not_found
-  end
+    def summary
+        movie = Movie.find(params[:id])
+        render json: movie, serializer: MovieSummarySerializer
+    end
+
+    private
+
+    def render_not_found_response
+        render json: { error: "Movie not found" }, status: :not_found
+    end
+
 end
